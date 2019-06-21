fffb93feaa055fd0c43caaf7d0cd53a5ced4a449
Chaitanya Chalasani <chaitanya@arknode.io>
2019-06-21 16:42:58 +0200

    Implemented tagging

---
+++ src/tivan_tags.erl
@@ -0,0 +1,59 @@
+%%%-------------------------------------------------------------------
+%%% @author danny
+%%% @copyright (C) 2019, danny
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2019-06-20 21:35:56.736576
+%%%-------------------------------------------------------------------
+-module(tivan_tags).
+
+%% API
+-export([create/1
+        ,tag/3
+        ,tags/2
+        ,entities/2]).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+create(Name) ->
+  tivan:create(Name, #{columns => [tag_entity, created_on]
+                       ,type => ordered_set}).
+
+tag(Name, Entity, Tag) ->
+  case tivan:get(Name, {Tag, Entity}) of
+    [] ->
+      Now = erlang:system_time(second),
+      tivan:put(Name, #{tag_entity => {Tag, Entity}, created_on => Now});
+    _ ->
+      ok
+  end.
+
+tags(Name, Entity) ->
+  TagEntities = tivan:get(Name, #{match => #{tag_entity => {2, 2, Entity}}
+                                   ,select => [tag_entity]}),
+  [ T || #{tag_entity := {T, _}} <- TagEntities ].
+
+entities(Name, [Tag]) ->
+  TagEntities = tivan:get(Name, #{match => #{tag_entity => {1, 2, Tag}}
+                                   ,select => [tag_entity]}),
+  [ E || #{tag_entity := {_, E}} <- TagEntities ];
+entities(Name, [Tag|Tags]) ->
+  lists:filter(
+    fun(Entity) ->
+        lists:all(
+          fun(T) ->
+              case tivan:get(Name, {T, Entity}) of
+                [] -> false;
+                _ -> true
+              end
+          end,
+          Tags
+         )
+    end,
+    entities(Name, [Tag])
+   ).
+
